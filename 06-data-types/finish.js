315db7511ce3909cbd257fd84aaeeed963765e66
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-03-21 14:52:04 +0300

    add: task 6 - типы данных

--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -12,3 +12,23 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const myName = 'Stas';
+const years = 18;
+const youOld = true;
+const myCountry = null; 
+const notInform = undefined;
+const myFilmsDB = {
+	triller: '1',
+	comedy: '2'
+}
+const myFilms = ["1", "2"];
+
+
+console.log(typeof(myName));
+console.log(typeof(years));
+console.log(typeof(youOld));
+console.log(typeof(myCountry));
+console.log(typeof(notInform));
+console.log(typeof(myFilmsDB));
+console.log(typeof(myFilms));
