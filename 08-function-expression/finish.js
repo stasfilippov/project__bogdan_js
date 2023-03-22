8ca5b64f640e26ed9bdab0956efacc394c6ac049
stasfilippov <kron888999@gmail.com>
2023-03-22 21:42:23 +0300

    add: task8 - function expression

--- 08-function-expression/finish.js
+++ 08-function-expression/finish.js
@@ -10,3 +10,10 @@
  *
  * 5. Результат вызовов функции выведите в консоль
  */
+
+const getHelloName = function (name) {
+    return `Привет, ${name}`;
+}
+
+console.log(getHelloName ('Stas'));
+console.log(getHelloName ('Petya'));
