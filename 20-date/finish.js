7700f89df0a7bf54fd0a38af97351fa2aaeca51e
stasfilippov <kron888999@gmail.com>
2023-03-24 22:26:58 +0300

    add: getTime - timestamp

--- 20-date/finish.js
+++ 20-date/finish.js
@@ -4,3 +4,7 @@
  *
  * 2. Выведите значение в консоль
  */
+
+let timeCurr = new Date ();
+let timeCurrInMilisec = timeCurr.getTime() // возвращает таймстамп - количество миллисекунд от 1 января 1970 года
+console.log(timeCurrInMilisec)
