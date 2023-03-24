7489771c11d9b19466dc755c75d942fbfc53eff3
stasfilippov <kron888999@gmail.com>
2023-03-24 21:39:31 +0300

    add: task 12 - create object

--- 12-create-object/finish.js
+++ 12-create-object/finish.js
@@ -8,3 +8,12 @@
  * 2. Выведите в консоль строку
  * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
  */
+
+const aboutMe = {
+    name: 'Stas',
+    surname: 'Filippov',
+    favoriteNumber: 7500
+}
+
+
+console.log(`My name is ${aboutMe.name} ${aboutMe.surname} and my favorite number is ${aboutMe.favoriteNumber}`);
