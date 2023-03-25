1d742f52a6c0c54d965122137841a343b9a3c8fb
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-03-25 18:05:07 +0300

    add: остаток от деления

--- 22-division-remainder/finish.js
+++ 22-division-remainder/finish.js
@@ -10,3 +10,11 @@
 
 const myNumber1 = 10 // 10 = 3 + 3 + 3 + 1
 const myNumber2 = 3
+
+console.log(myNumber1 % myNumber2)
+
+const a = 10 // 10 = 3 + 3 + 3 + 1
+const b = 6
+const num3 = 3
+
+console.log((a % b) % num3)
